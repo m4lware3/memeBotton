a7a991316e5377f64ad3bcc2c6ac376c2df9cf64
odin <0x1dfc@protonmail.com>
2019-04-09 13:22:12 -0300

    Start

---
+++ app/src/main/java/com/example/memebotton/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.memebotton;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        v.getId();
+
+    }
+}
